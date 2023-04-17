85f8dba43317b4f98ffa0fbf1a97d36aaaf50230
runhuasun <127193621+runhuasun@users.noreply.github.com>
2023-04-17 18:36:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,21 @@
 
 import argparse
 import config
-from channel import channel_factory
+import psycopg2
 from common import log, const
 from multiprocessing import Pool
 
 
 # 启动通道
-def start_process(channel_type, config_path):
+def start_process(config_path):
     try:
         # 若为多进程启动,子进程无法直接访问主进程的内存空间,重新创建config类
         config.load_config(config_path)
-        model_type = config.conf().get("model").get("type")
-        log.info("[MultiChannel] Start up {} on {}", model_type, channel_type)
-        channel = channel_factory.create_channel(channel_type)
-        channel.startup()
+
+        # 执行任务的逻辑
+        
     except Exception as e:
-        log.error("[MultiChannel] Start up failed on {}: {}", channel_type, str(e))
+        log.error("进程发生错误", str(e))
 
 
 def main():
@@ -25,26 +24,7 @@ def main():
         # load config
         config.load_config(args.config)
 
-        model_type = config.conf().get("model").get("type")
-        channel_type = config.conf().get("channel").get("type")
 
-        # 1.单个字符串格式配置时，直接启动
-        if not isinstance(channel_type, list):
-            start_process(channel_type, args.config)
-            exit(0)
-
-        # 2.单通道列表配置时，直接启动
-        if len(channel_type) == 1:
-            start_process(channel_type[0], args.config)
-            exit(0)
-
-        # 3.多通道配置时，进程池启动
-        # 使用主进程启动终端通道
-        if const.TERMINAL in channel_type:
-            index = channel_type.index(const.TERMINAL)
-            terminal = channel_type.pop(index)
-        else:
-            terminal = None
 
         # 使用进程池启动其他通道子进程
         pool = Pool(len(channel_type))
